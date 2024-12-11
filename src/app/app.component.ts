fdde96a0f59039f360e09f120371876816536e77
Michal Grzegorczyk <michalgrzegorczyk.dev@gmail.com>
2024-12-11 19:51:37 +0100

    tesst

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,44 @@ import {Component, AfterViewInit, ElementRef, inject, viewChild} from '@angular/
 import {QuillIntegrationService} from "./editor/quill-integration.service";
 import Quill from 'quill';
 import {BlockDivBlot} from "./editor/blots/block-div.blot";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   template: `
-    <div #editor></div>
-    <button class="bbb" (click)="addBlock()">Add line</button>
-    <button class="bbb" (click)="loadTestData()">LOAD TEST DATA</button>
+    <div style="border: 1px solid gray; padding: 15px">
+      <h3 style="margin: 0;">Development Helpers:</h3>
+
+      <button style="margin-right: 10px; margin-top: 10px" (click)="addBlock()">Add new line</button>
+      <button class="bbb" (click)="loadTestData()">Reset content</button>
+    </div>
+    <div style="display: flex">
+      <div style="width: 1000px; padding: 15px; border: 1px solid gray; border-top: none; border-right: none">
+        <div #editor></div>
+      </div>
+      <div style="width: 1000px; border: 1px solid gray; border-top: none; padding: 15px">
+        <h3 style="margin: 0;">Store:</h3>
+        <pre>
+          <code>
+{{ content | json }}
+          </code>
+        </pre>
+      </div>
+    </div>
   `,
+  imports: [
+    JsonPipe
+  ]
 })
 export class AppComponent implements AfterViewInit {
   private rootEditorElement = viewChild<ElementRef>('editor');
   private readonly quillIntegrationService = inject(QuillIntegrationService);
+  content = {
+    blocks: [
+      "no store lol"
+    ]
+  };
 
   ngAfterViewInit(): void {
     Quill.register(BlockDivBlot, true);
